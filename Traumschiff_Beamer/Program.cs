99a1676cfa9d09e3867f53495dc491ce5b1bc339
kstAtBitLC <klaus.stolz@bitlc.de>
2020-03-04 09:59:34 +0100

    Add project files.

---
+++ Traumschiff_Beamer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Traumschiff_Beamer {
+    class Program {
+        static void Main ( string [] args ) {
+            Console.WriteLine ("Traumschiff");
+
+            Traumschiff germania = new Traumschiff () { Name = "Germania" };
+            Traumschiff persia = new Traumschiff () { Name = "Persia" };
+
+            Thread germaniaThread = new Thread ( new ThreadStart(germania.Herumfahren));
+            Thread persiaThread = new Thread ( new ThreadStart(persia.Herumfahren));
+
+            //germaniaThread.Start ();
+            persiaThread.Start ();
+
+
+            //for ( int i = 0 ; i < 10 ; i++ ) {
+            //    germania.Herumfahren ();
+            //    persia.Herumfahren ();
+            //    Thread.Sleep ( 1000 );
+            //    Console.Clear ();
+            //}
+            
+
+            Console.ReadLine ();
+        }
+    }
+}
---
+++ Traumschiff_Beamer/Traumschiff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Traumschiff_Beamer {
+    class Traumschiff {
+        public string Name { get; set; }
+        public Koordinate Koordinate { get; set; }
+        private Random zfg = new Random ();
+
+        public void Herumfahren () {
+            while ( true ) {
+                this.Koordinate = BeschaffeNeueZielKoordinaten ();
+                this.SchiffAnzeigen (Name);
+                Thread.Sleep (3000);
+            }
+        }
+
+        public void SchiffAnzeigen (string name) {
+            Console.SetCursorPosition( this.Koordinate.X, this.Koordinate.Y );
+            Console.Write ("-^-"+name);
+        }
+
+        public Koordinate BeschaffeNeueZielKoordinaten () {
+            Koordinate k = new Koordinate ();
+            int x = zfg.Next ( 0, 81 );
+            int y = zfg.Next (0,26);
+            k.X = x;
+            k.Y = y;
+            return k;
+        }
+
+    }
+}
